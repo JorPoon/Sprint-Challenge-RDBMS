228331e5230b5767982a3dded2ade6dba6dc463e
Jor_Poon <jormpoon@gmail.com>
2019-03-29 12:03:41 -0400

    created actions table  in migration-project-table

--- data/migrations/20190329114103_project_table.js
+++ data/migrations/20190329114103_project_table.js
@@ -1,8 +1,59 @@
 
 exports.up = function(knex, Promise) {
+  return knex.schema
   
+    .createTable('projects', tbl => {
+        //primary key, auto increments
+        tbl.increments();
+
+        //project name - required - unique
+        tbl
+         .string('name', 128)
+         .notNullable()
+         .unique();
+        
+         //description - required 
+        tbl
+         .string('description', 128)
+         .notNullable();
+
+        //completed: boolean
+        tbl
+         .boolean('completed');
+  })
+  .createdTable('actions', tbl => {
+      //primary key , auto increments
+      tbl.increments();
+
+      //action-description
+      tbl
+       .string('description', 128)
+       .notNullable();
+    
+     //action-notes
+      tbl
+       .string('notes', 255)
+       .notNullable();
+
+     //action-completed:boolean
+      tbl
+       .boolean('completed')  
+
+    //connect to projects table
+      tbl
+       .integer('project_id')
+       .unsigned()
+       .notNullable()
+       .references('id')
+       .inTable('projects')
+       .onDelete('CASCADE')
+       .onUpdate('CASCADE')
+  })
 };
 
 exports.down = function(knex, Promise) {
+    return knex.schema
+        .dropTableIfExists('projects')
+        .dropTableIfExists('actions');
   
 };
